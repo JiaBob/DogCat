77f3e936f7d0ea222e8718b74b91dfec08644c7a
Jiabo <123419282@qq.com>
2018-10-19 22:07:19 +1100

    perfect file name extraction

--- dataloader.py
+++ dataloader.py
@@ -1,23 +1,20 @@
-import torch
-import numpy as np
 from torch.utils.data import Dataset
 from PIL import Image
-import glob, re
+import glob, re, os
 
 class DogCat(Dataset):
     def __init__(self, path, transform):
         self.img_paths = glob.glob(path)
         self.cls = {'cat': 0, 'dog': 1}
         self.transforms = transform
-        print(self.img_paths[0])
 
     def __len__(self):
         return len(self.img_paths)
 
     def __getitem__(self, i):
-        img_name = self.img_paths[i]
-        label = re.search(r'.*/(.*)\.\d+', img_name).group(1)
-        print(img_name, label)
-        img = Image.open(img_name)
+        img_path = self.img_paths[i]
+        _, img_name = os.path.split(img_path)  # split file path and file name. dont use re to do it.
+        label = re.search(r'(.*)\.\d+', img_name).group(1)  # windows use '/', linux use '\' to split address
+        img = Image.open(img_path)
         img = self.transforms(img)
         return img, self.cls[label]
